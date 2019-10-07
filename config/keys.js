1b189f406e3c8e7c4dd9a2e5287e647e17a07ebd
Hameed Damee <dameeimpact@gmail.com>
2019-10-07 09:30:35 +0100

    Oauth Setup, db setup

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
-}
+  mongoURI:
+    "mongodb+srv://hameedDamee:hameedDamee@cluster0-1qxmu.mongodb.net/whistleblower?retryWrites=true&w=majority",
+  googleClientID:
+    "283070590852-4h91ie11e7pk8763fd7gns5dgoe1tkoj.apps.googleusercontent.com",
+  googleClientSecret: "cnVf180w-MvMdhkP_7fVz9xp"
+};
