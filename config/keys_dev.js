1d4d0c887aebc05ab542b6c60a9b2816ae06a7d5
Hameed Damee <dameeimpact@gmail.com>
2019-10-14 11:15:40 +0100

    Update on Credentials

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: "mongodb://localhost/bizjot-dev",
-  googleClientID: "CHANGEME",
-  googleClientSecret: "CHANGEME"
+  googleClientID:
+    "283070590852-4h91ie11e7pk8763fd7gns5dgoe1tkoj.apps.googleusercontent.com",
+  googleClientSecret: "cnVf180w-MvMdhkP_7fVz9xp"
 };
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://hameedDamee:hameedDamee@cluster0-1qxmu.mongodb.net/whistleblower?retryWrites=true&w=majority",
-  googleClientID:
-    "283070590852-4h91ie11e7pk8763fd7gns5dgoe1tkoj.apps.googleusercontent.com",
-  googleClientSecret: "cnVf180w-MvMdhkP_7fVz9xp"
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
